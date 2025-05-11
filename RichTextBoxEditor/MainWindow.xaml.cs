8d375f3d1fae3e37e19bb1720e49f1eedcd4fce4
StodolaR <sto.dy@seznam.cz>
2025-05-11 19:43:15 +0200

    Prikazy Bold, Italic, Underline, FontFamily a FontSize

--- RichTextBoxEditor/MainWindow.xaml.cs
+++ RichTextBoxEditor/MainWindow.xaml.cs
@@ -24,14 +24,22 @@ namespace RichTextBoxEditor
     {
         private string filePath;
         private bool edited;
+        private bool firstEdit;
         
         public MainWindow()
         {
             InitializeComponent();
             filePath = string.Empty;
             edited = false;
+            firstEdit = true;
+            cbFFamily.ItemsSource = Fonts.SystemFontFamilies;
+            cbFSize.ItemsSource = new double[] { 8, 10, 12, 16, 20, 24, 32, 40, 48 };
+            rtbEditor.Focus();
+            ActualizeButtonsStates();
         }
+
         // MemuItem Soubor
+
         // MenuItem Novy
         private void CbNew_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -62,12 +70,38 @@ namespace RichTextBoxEditor
             }
             return false;
         }
-
         private void RtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
         {
             edited = true;
+            ActualizeButtonsStates();
         }
-
+        private void ActualizeButtonsStates()
+        {
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            if (temp == DependencyProperty.UnsetValue)
+            {
+                cbFFamily.Text = string.Empty;
+            }
+            else
+            {
+                cbFFamily.Text = temp.ToString();
+            }
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            if (temp == DependencyProperty.UnsetValue)
+            {
+                cbFSize.Text = string.Empty;
+            }
+            else
+            {
+                cbFSize.Text = temp.ToString();
+            }
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = miBold.IsChecked = temp != DependencyProperty.UnsetValue && temp.Equals(FontWeights.Bold);
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = miItalic.IsChecked = temp != DependencyProperty.UnsetValue && temp.Equals(FontStyles.Italic);
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = miUnderline.IsChecked = temp != DependencyProperty.UnsetValue && temp.Equals(TextDecorations.Underline);
+        }
         private void AddToLastDocs()
         {
             for (int i = 0 ; i < miLastDoc.Items.Count; i++)
@@ -92,6 +126,7 @@ namespace RichTextBoxEditor
             lastDoc.Icon = icon;
             miLastDoc.Items.Insert(0, lastDoc);
         }
+
         //Menuitem Otevrit
         private void CbOpen_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -128,6 +163,7 @@ namespace RichTextBoxEditor
                 MessageBox.Show("Dokument nelze načíst" + Environment.NewLine + ex);
             }
         }
+
         //MenuItem Posledni dokumenty
         private void CbOpenLast_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -138,6 +174,7 @@ namespace RichTextBoxEditor
             string openFilePath = (string)((MenuItem)sender).Header;
             OpenFile(openFilePath);
         }
+
         //MenuItem Ulozit
         private void CbSave_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -155,6 +192,7 @@ namespace RichTextBoxEditor
                 SaveAs();
             }
         }
+
         //MenuItem Ulozit jako
         private void CbSaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -190,6 +228,7 @@ namespace RichTextBoxEditor
                 MessageBox.Show("Dokument nelze uložit" + Environment.NewLine + ex);
             }
         }
+
         //MenuItem Vlastnosti
         private void CbProperties_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -217,6 +256,7 @@ namespace RichTextBoxEditor
                           + $"Velikost            : {size}bytů" + Environment.NewLine
                           + $"Vytvořen          : {createDate}" + Environment.NewLine);
         }
+
         //MenuItem Konec
         private void CbClose_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -228,6 +268,7 @@ namespace RichTextBoxEditor
         }
 
         //MenuItem Upravy
+
         //MenuItem Zpet
         private void CbUndo_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
@@ -236,17 +277,17 @@ namespace RichTextBoxEditor
                 e.CanExecute = rtbEditor.CanUndo;
             }
         }
-
         private void CbUndo_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             rtbEditor.Undo();
         }
-
         private void RtbEditor_KeyUp(object sender, KeyEventArgs e)
         {
-            rtbEditor.Undo();
-            rtbEditor.Redo();
+            //TODO Undo Redo
+            //rtbEditor.Undo();
+            //rtbEditor.Redo();
         }
+
         //MenuItem Vpred
         private void CbRedo_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
@@ -255,23 +296,25 @@ namespace RichTextBoxEditor
                 e.CanExecute = rtbEditor.CanRedo;
             }
         }
-
         private void CbRedo_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             rtbEditor.Redo();
         }
-        //MenuItem Najdi
+
+        //MenuItem Najit
         private void CbFind_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             spFindAndReplace.Visibility = Visibility.Visible;
             btnStartFind.Visibility = Visibility.Visible;
+            btnStartReplace.Visibility = Visibility.Collapsed;
+            tbxReplace.Visibility = Visibility.Collapsed;
+            tbxReplace.Text = string.Empty;
+            tbReplace.Visibility = Visibility.Collapsed;
         }
-
         private void BtnStartFind_Click(object sender, RoutedEventArgs e)
         {
             FindOrReplacePattern(false);
         }
-
         private void FindOrReplacePattern(bool replace)
         {
             TextRange allText = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
@@ -307,54 +350,149 @@ namespace RichTextBoxEditor
             }
 
         }
-
         private void BtnCloseFind_Click(object sender, RoutedEventArgs e)
         {
-            spFindAndReplace.Visibility=Visibility.Collapsed;
-            tbReplace.Visibility=Visibility.Collapsed;
+            CloseFindAndReplace();
+        }
+        private void CloseFindAndReplace()
+        {
+            spFindAndReplace.Visibility = Visibility.Collapsed;
+            tbReplace.Visibility = Visibility.Collapsed;
             tbxReplace.Visibility = Visibility.Collapsed;
-            btnStartFind.Visibility=Visibility.Collapsed;
+            btnStartFind.Visibility = Visibility.Collapsed;
             btnStartReplace.Visibility = Visibility.Collapsed;
             tbxFind.Text = string.Empty;
             tbxReplace.Text = string.Empty;
             tbStatus.Text = string.Empty;
         }
-
         private void RtbEditor_GotFocus(object sender, RoutedEventArgs e)
         {
             TextRange allText = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
             allText.ApplyPropertyValue(TextElement.BackgroundProperty, new SolidColorBrush(Colors.White));
         }
-        //MenuItem Zamen
+
+        //MenuItem Nahradit
         private void CbReplace_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             spFindAndReplace.Visibility = Visibility.Visible;
             tbReplace.Visibility = Visibility.Visible;
             tbxReplace.Visibility = Visibility.Visible;
             btnStartReplace.Visibility=Visibility.Visible;
+            btnStartFind.Visibility=Visibility.Collapsed; 
         }
-
         private void BtnStartReplace_Click(object sender, RoutedEventArgs e)
         {
             FindOrReplacePattern(true);
         }
-        //Najdi a zamen - Oznaceni textu v textboxech pri ziskani focusu
+        //Najit a nahradit - Oznaceni textu v textboxech pri ziskani focusu
         private void TextBox_LostMouseCapture(object sender, MouseEventArgs e)
         {
             TextBox textBox = (TextBox)sender;
             textBox.SelectAll();
             textBox.LostMouseCapture -= TextBox_LostMouseCapture;
         }
-
         private void TextBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             TextBox textBox = (TextBox)sender;
             textBox.LostMouseCapture += TextBox_LostMouseCapture;
         }
 
-        
-        
+        //MenuItem Format
 
+        //Menuitem Pismo
+        //Menuitem Tucne
+        private void rtbEditor_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!firstEdit) return;
+            Run run = new Run(e.Text, rtbEditor.CaretPosition);
+            rtbEditor.BeginChange();
+            if (btnBold.IsChecked == true)
+            {
+                run.FontWeight = FontWeights.Bold;
+            }
+            if (btnItalic.IsChecked == true)
+            {
+                run.FontStyle = FontStyles.Italic;
+            }
+            if (btnUnderline.IsChecked == true)
+            {
+                run.TextDecorations = TextDecorations.Underline;
+            }
+            if (cbFFamily.SelectedItem != null)
+            {
+                rtbEditor.FontFamily = (FontFamily)cbFFamily.SelectedItem;
+            }
+            if (double.TryParse(cbFSize.Text, out double value))
+            {
+                rtbEditor.FontSize = value;
+            }
+            rtbEditor.EndChange();
+            e.Handled = true;
+            rtbEditor.CaretPosition = run.ElementEnd;
+            firstEdit = false;
+        }
+        private void miBold_Click(object sender, RoutedEventArgs e)
+        {
+            btnBold.IsChecked = miBold.IsChecked;
+        }
+        private void btnBold_Click(object sender, RoutedEventArgs e)
+        {
+            if(btnBold.IsChecked != null)
+            {
+                miBold.IsChecked = (bool)btnBold.IsChecked;
+            }
+        }
 
+        //MenuItem Kurziva
+        private void miItalic_Click(object sender, RoutedEventArgs e)
+        {
+            btnItalic.IsChecked = miItalic.IsChecked;
+        }
+        private void btnItalic_Click(object sender, RoutedEventArgs e)
+        {
+            if(btnItalic.IsChecked != null)
+            {
+                miItalic.IsChecked= (bool)btnItalic.IsChecked;
+            }
+        }
+
+        //MenuItem Podtrzene
+        private void miUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            btnUnderline.IsChecked = miUnderline.IsChecked;
+        }
+        private void btnUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnUnderline.IsChecked != null)
+            {
+                miUnderline.IsChecked= (bool)btnUnderline.IsChecked;
+            }
+        }
+
+        //Combobox FontFamily
+        private void cbFFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(FontFamilyProperty, cbFFamily.SelectedItem);
+            rtbEditor.Focus();
+        }
+
+        //Combobox FontSize
+        private void cbFSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(FontSizeProperty, cbFSize.SelectedItem);
+            rtbEditor.Focus();
+        }
+
+        private void cbFSize_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (double.TryParse(cbFSize.Text, out double value))
+                {
+                    rtbEditor.Selection.ApplyPropertyValue(FontSizeProperty, value);
+                }
+                rtbEditor.Focus();
+            }
+        }        
     }
 }
